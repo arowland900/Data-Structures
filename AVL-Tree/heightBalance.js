fa0a3eefd7ebce2c64f7623b08805fda89b1b6d5
Alexander Rowland <arowland900@gmail.com>
2021-06-11 03:03:56 -0700

    add isBalanced function

--- AVL-Tree/heightBalance.js
+++ AVL-Tree/heightBalance.js
@@ -8,3 +8,18 @@ function height(root){
     if(!root) return 0
     return Math.max(height(root.left), height(root.right)) + 1
 }
+
+function isBalanced(root){
+    if(!root) return true
+
+    let l = height(root.left)
+    let r = height(root.right)
+
+    if( Math.abs(l - r) <= 1 
+        && isBalanced(root.left)
+        && isBalanced(root.right)){
+            return true
+        }
+    
+    return false
+}
